8e21082aa755bc34d1ca2f5990e7897637cdc174
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-06-15 03:06:23 +0000

    Change CatalogItemState to use Dict vs List

--- src/eShopOnBlazorWasm/Source/Client/Features/CatalogItem/Components/Details.razor.cs
+++ src/eShopOnBlazorWasm/Source/Client/Features/CatalogItem/Components/Details.razor.cs
@@ -1,15 +1,11 @@
 ﻿namespace eShopOnBlazorWasm.Features.CatalogItems.Components
 {
   using Microsoft.AspNetCore.Components;
-  using System;
-  using System.Collections.Generic;
-  using System.Linq;
-  using System.Text;
-  using System.Threading.Tasks;
   public partial class Details
   {
     public CatalogItemDto CatalogItem => 
-      CatalogItemState.CatalogItems.SingleOrDefault(aCatalogItemDto => aCatalogItemDto.Id == CatalogItemId);
+      CatalogItemState.CatalogItems[CatalogItemId];
+
     [Parameter] public int CatalogItemId { get; set; }
 
     public string CatalogBrand => CatalogBrandState.CatalogBrands[CatalogItem.CatalogBrandId].Brand;
