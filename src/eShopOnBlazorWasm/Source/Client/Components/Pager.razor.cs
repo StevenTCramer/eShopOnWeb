dc7fa2de00942765fc48ecde802855efbfb6371c
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-06-15 05:00:38 +0000

    Rename Pagination to Pager

---
+++ src/eShopOnBlazorWasm/Source/Client/Components/Pager.razor.cs
@@ -0,0 +1,18 @@
+﻿namespace eShopOnBlazorWasm.Components
+{
+  using eShopOnBlazorWasm.Features.Bases;
+  using Microsoft.AspNetCore.Components;
+  using System.Threading.Tasks;
+
+  public partial class Pager:BaseComponent
+  {
+    [Parameter] public EventCallback<int> OnPageChange { get; set; }
+    [Parameter] public int PageCount { get; set; }
+    [Parameter] public int PageIndex { get; set; }
+
+    private async Task OnClick(int aPageIndex)
+    {
+      await OnPageChange.InvokeAsync(aPageIndex);
+    }
+  }
+}
