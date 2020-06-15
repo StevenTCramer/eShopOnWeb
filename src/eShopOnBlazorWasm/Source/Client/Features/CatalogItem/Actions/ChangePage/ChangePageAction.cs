6465e94b30c908a07181ffbd0347dba2f9dfa86d
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-06-15 05:00:53 +0000

    Implement ChangePageAction

---
+++ src/eShopOnBlazorWasm/Source/Client/Features/CatalogItem/Actions/ChangePage/ChangePageAction.cs
@@ -0,0 +1,12 @@
+﻿namespace eShopOnBlazorWasm.Features.CatalogItems
+{
+  using eShopOnBlazorWasm.Features.Bases;
+
+  internal partial class CatalogItemState
+  {
+    public class ChangePageAction : BaseAction 
+    {
+      public int  PageIndex { get; set; }
+    }
+  }
+}
---
+++ src/eShopOnBlazorWasm/Source/Client/Features/CatalogItem/Actions/ChangePage/ChangePageHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace eShopOnBlazorWasm.Features.CatalogItems
+{
+  using BlazorState;
+  using eShopOnBlazorWasm.Features.Bases;
+  using MediatR;
+  using System.Threading;
+  using System.Threading.Tasks;
+  internal partial class CatalogItemState
+  {
+    public class ChangePageHandler : BaseHandler<ChangePageAction>
+    {
+      public ChangePageHandler(IStore aStore) : base(aStore) { }
+      
+      public override Task<Unit> Handle(ChangePageAction aChangePageAction, CancellationToken aCancellationToken)
+      {
+        CatalogItemState.PageIndex = aChangePageAction.PageIndex;
+        return Unit.Task;
+      }
+    }
+  }
+}
