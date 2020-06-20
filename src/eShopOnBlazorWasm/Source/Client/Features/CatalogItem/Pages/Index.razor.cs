4ef328fbf9e3a1c945eb534d09e18a6a187e3aa3
StevenTCramer <Steven.Cramer@TimeWarp.Enterprises>
2020-06-20 16:09:09 +0000

    Move CatalogItem to Page

--- src/eShopOnBlazorWasm/Source/Client/Features/CatalogItem/Pages/Index.razor.cs
+++ src/eShopOnBlazorWasm/Source/Client/Features/CatalogItem/Pages/Index.razor.cs
@@ -4,11 +4,25 @@ namespace eShopOnBlazorWasm.Features.CatalogItems.Pages
   using eShopOnBlazorWasm.Features.Bases;
   using System.Threading.Tasks;
 
-  public partial class Index: BaseComponent
+  using static eShopOnBlazorWasm.Features.CatalogItems.CatalogItemState;
+
+  public partial class Index : BaseComponent
   {
     public const string Route = "/Catalog";
 
     protected async Task CreateClick() =>
       _ = await Mediator.Send(new RouteState.ChangeRouteAction { NewRoute = Create.Route });
+
+    protected async Task HandlePageChange(int aPageIndex)
+    {
+      _ = await Mediator.Send(new ChangePageAction { PageIndex = aPageIndex });
+      _ = await Mediator.Send(new FetchCatalogItemsAction());
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+      _ = await Mediator.Send(new FetchCatalogItemsAction());
+      await base.OnInitializedAsync();
+    }
   }
 }
